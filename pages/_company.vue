21e56ec515d510f78ae9fc6c3ba25d94f0c04fbd
Leo Reuter <leo.reuter@i22.de>
2020-03-22 00:37:42 +0100

    feature: updated company page

--- pages/_company.vue
+++ pages/_company.vue
@@ -1,17 +1,38 @@
 <template>
-  <v-layout
-    justify-center
-  >
-    <v-flex
-      xs12
-      sm10
-      md8
-    >
-      <div class="text-center">
-        {{ company.headline }}
-      </div>
-    </v-flex>
-  </v-layout>
+  <v-container fluid>
+    <v-row justify="space-around">
+      <v-col cols="12">
+        <v-img src="https://i.picsum.photos/id/299/1300/300.jpg" />
+      </v-col>
+    </v-row>
+    <v-row justify="space-around">
+      <v-col
+        cols="12"
+        sm="8"
+        md="5"
+        offset-sm="2"
+        offset-md="3"
+      >
+        <h1 class="headline mb-10">
+          {{ company.headline }}
+        </h1>
+
+        <p class="block-1">
+          {{ company.description }}
+        </p>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="2"
+        offset-md="1"
+        md="3"
+      >
+        <v-btn primary class="pl-10 pr-10">
+          Spenden
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
