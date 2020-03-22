0615860d77c289d7a28e6f1296caf7b077741eb0
Henry Steinhauer <henry.steinhauer@i22.de>
2020-03-22 13:17:31 +0100

    edit end page

--- pages/successfullyPage.vue
+++ pages/successfullyPage.vue
@@ -7,11 +7,16 @@
       xs12
       sm10
       md8
+      justify-center
+      flex-column
+      class="container"
     >
-      <v-icon>{{ mdiCheck }}</v-icon>
-      <p>
+      <v-avatar size="126" class="avatar pa-12 mb-12 mx-auto">
+        <v-icon color="teal">{{ mdiCheck }}</v-icon>
+      </v-avatar>
+      <p class="content display-1 ma-6">
         Danke!<br>
-        Deine Spende ist angekommen.
+        Deine Spende <nobr>ist angekommen.</nobr>
       </p>
     </v-flex>
   </v-layout>
@@ -28,3 +33,18 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  max-width: 550px;
+
+  .avatar {
+    border: 1px solid teal;
+  }
+  .content {
+    color: teal;
+    max-width: 500px;
+  }
+}
+</style>
