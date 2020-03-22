36cc6e7dd0e1087f134b1a4833bcde75f8693702
Leo Reuter <leo.reuter@i22.de>
2020-03-22 19:29:51 +0100

    feature: donation popup

---
+++ components/Donation.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-layout
+    justify-center
+    align-center
+    class="donation"
+  >
+    <v-icon color="#000" width="60" large class="donation-icon" @click="$emit('close')">
+      {{ mdiClose }}
+    </v-icon>
+    <v-flex
+      xs12
+      sm10
+      md8
+    >
+      <h1>So kannst du spenden:</h1>
+      <v-container class="mt-6">
+        <Donate-Element :payment-info="payments" payment-type="paypal" />
+        <Donate-Element :payment-info="payments" payment-type="gofoundme" />
+        <Donate-Element :payment-info="payments" payment-type="bank" />
+      </v-container>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import { mdiClose } from '@mdi/js'
+import DonateElement from '~/components/DonateElement'
+
+export default {
+  components: {
+    DonateElement
+  },
+  props: {
+    payments: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data () {
+    return {
+      mdiClose
+    }
+  }
+}
+</script>
+
+<style>
+.donation {
+  position: relative;
+}
+
+.donation-icon {
+  position: absolute !important;
+  right: 20px;
+  top: 20px;
+  cursor: pointer;
+}
+</style>
