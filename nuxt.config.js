a83a2a12d3330095e2f71c4bd6bb34906bf6506f
Leo Reuter <leo.reuter@i22.de>
2020-03-22 18:08:48 +0100

    vuetify tree shaking

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,6 +67,7 @@ export default {
   ** https://github.com/nuxt-community/vuetify-module
   */
   vuetify: {
+    treeShake: true,
     customVariables: ['~/assets/variables.scss'],
     theme: {
       dark: false,
