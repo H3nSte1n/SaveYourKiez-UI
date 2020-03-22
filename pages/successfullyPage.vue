20149c1e842129cc04769641c40981a1681723ae
Henry Steinhauer <henry.steinhauer@i22.de>
2020-03-22 12:24:02 +0100

    create end Page

---
+++ pages/successfullyPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm10
+      md8
+    >
+      <v-icon>{{ mdiCheck }}</v-icon>
+      <p>
+        Danke!<br>
+        Deine Spende ist angekommen.
+      </p>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import { mdiCheck } from '@mdi/js'
+
+export default {
+  data () {
+    return {
+      mdiCheck
+    }
+  }
+}
+</script>
